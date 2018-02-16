27586a9fd24fbb4eafc68fef0c34f160372a820a
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2018-02-16 13:56:19 +0200

    gitignore + create and populate matrix

--- lab1.c
+++ lab1.c
@@ -1,10 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "omp.h"
 
 int main() {
-#pragma omp parallel
-    {
-        printf("hello world \n");
-    }
+
+        srand((unsigned int)time(NULL));
+
+        int dimensions[]={4,4,4};
+
+        // Calculate size of required 1D matrix
+        int maxSize=1;
+        for (int i=0;i<sizeof(dimensions)/sizeof(dimensions[0]);i++)
+        {
+            maxSize=maxSize*dimensions[i];
+           // printf("%i \n",maxSize);
+        }
+
+        // Create a 1D matrix containing random data 
+        float flatArray[maxSize];
+        float a=100;
+        for (int i=0; i<maxSize; i++)
+        {
+            flatArray[i] = ((float)rand()/(float)(RAND_MAX)) * a;
+            printf("%f \n",flatArray[i]);
+        }
+
+
+
+    
 }
