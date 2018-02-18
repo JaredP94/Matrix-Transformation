d27fee8e1161934696542fa054a29b8592b693e9
Jared Ping <704447@students.wits.ac.za>
2018-02-18 16:42:30 +0200

    Functional solution - no threading implementation

--- lab1.c
+++ lab1.c
@@ -3,30 +3,72 @@
 #include <time.h>
 #include "omp.h"
 
+struct arrayContainer
+{
+    int * _array_ptr;
+    int _array_capacity;
+};
+
+struct arrayContainer generateArray(int dimensions[]){
+    int _capacity = 1;
+    srand((unsigned int)time(NULL));
+
+    for (int i = 0; i < sizeof(dimensions)/sizeof(int); i++)
+    {
+        _capacity = _capacity * dimensions[i];
+    }
+
+    static int * _created_array;
+    _created_array =(int*)malloc(_capacity * sizeof(int));
+
+    for (int i = 0; i < _capacity; i++)
+    {
+        _created_array[i] = rand() % 10;
+    }
+
+    struct arrayContainer arrayInfo = {_created_array, _capacity};
+
+    return arrayInfo;
+}
+
+struct arrayContainer initializeZero(struct arrayContainer arrayInfo){
+    for (int i = 0; i < arrayInfo._array_capacity; i++)
+    {
+        arrayInfo._array_ptr[i] = 0;
+    }
+
+    return arrayInfo;
+}
+
+struct arrayContainer uniformOne(struct arrayContainer arrayInfo){
+    int _amount_to_set = arrayInfo._array_capacity / 10;
+    int _spacing = arrayInfo._array_capacity / _amount_to_set;
+
+    for (int i = 0; i < _amount_to_set; i++)
+    {
+        arrayInfo._array_ptr[i * _spacing] = 1;
+    }
+
+    return arrayInfo;
+}
+
+struct arrayContainer uniformRandomOne(struct arrayContainer arrayInfo, int dimensions[]){
+    int _amount_to_set = arrayInfo._array_capacity / 20;
+    int _spacing = arrayInfo._array_capacity / _amount_to_set;
+    int _random_index = rand() % dimensions[0];
+
+    for (int i = 0; i < _amount_to_set; i++)
+    {
+        arrayInfo._array_ptr[_random_index + i * _spacing] = 1;
+    }
+
+    return arrayInfo;
+}
+
 int main() {
-
-        srand((unsigned int)time(NULL));
-
-        int dimensions[]={4,4,4};
-
-        // Calculate size of required 1D matrix
-        int maxSize=1;
-        for (int i=0;i<sizeof(dimensions)/sizeof(dimensions[0]);i++)
-        {
-            maxSize=maxSize*dimensions[i];
-           // printf("%i \n",maxSize);
-        }
-
-        // Create a 1D matrix containing random data 
-        float flatArray[maxSize];
-        float a=100;
-        for (int i=0; i<maxSize; i++)
-        {
-            flatArray[i] = ((float)rand()/(float)(RAND_MAX)) * a;
-            printf("%f \n",flatArray[i]);
-        }
-
-
-
-    
+        int _dimensions[]={50,50};
+        struct arrayContainer _generated_array = generateArray(_dimensions);
+        _generated_array = initializeZero(_generated_array);
+        _generated_array = uniformOne(_generated_array);
+        _generated_array = uniformRandomOne(_generated_array, _dimensions);
 }
